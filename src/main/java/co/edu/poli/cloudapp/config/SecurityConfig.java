1d97f866bea0c5e084c126c4b973ffda3a0c3ec2
Disco <lululfgd@gmail.com>
2025-12-11 14:12:33 -0500

    cloudapp

---
+++ src/main/java/co/edu/poli/cloudapp/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package co.edu.poli.cloudapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+
+public class SecurityConfig {
+
+}
